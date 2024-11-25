de7fa199d95ac4d21b0b6d8ee8625d4e6fa93edd
nesma <nesma2020s120@gmail.com>
2024-11-25 03:10:15 -0800

    splash and onbording screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:final_project/onbording_screen.dart'; // استيراد صفحة Onboarding
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // الانتقال إلى صفحة Onboarding بعد 3 ثوانٍ
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // الشعار (يمكنك استبدال الصورة بالشعار الموجود في التصميم)
+            Image.asset(
+              'assets/images/logo.PNG', // استبدليه بمسار اللوجو الصحيح
+              height: 120,
+            ),
+            const SizedBox(height: 400),
+            // دائرة التحميل (تدور لفترة قصيرة فقط)
+            const CircularProgressIndicator(
+              color: Colors.purple,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
